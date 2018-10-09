577ebe92334a23c1622ca61d8ec95ec8a2272a72
d于洪 <hyu@coremail.cn>
2018-10-09 16:28:33 +0800

    chore(vue): 集成Vue

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<!--
+  - Copyright (c) 2018 Mailtech.cn, Ltd. All Rights Reserved.
+  -->
+
+<template>
+    <div id="app">
+        {{name}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data: () => ({
+        name: 'hello world',
+    }),
+    mounted() {
+    },
+}
+</script>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-import _ from 'lodash';
-import printMe from './print.js';
+/*
+ * Copyright (c) 2018 Mailtech.cn, Ltd. All Rights Reserved.
+ */
 
-function component() {
-  var element = document.createElement('div');
-    var btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
-    return element;
-    
-  }
-  let element = component(); // 当 print.js 改变导致页面重新渲染时，重新获取渲染的元素
-  document.body.appendChild(element);
-   if (module.hot) {
-       module.hot.accept('./print.js', function() {
-         console.log('Accepting the updated printMe module!');
-         document.body.removeChild(element);
-     element = component(); // 重新渲染页面后，component 更新 click 事件处理
-     document.body.appendChild(element);
-       })
-     }
+import Vue from 'vue';
+import App from 'app';
+  /** app init */
+  new Vue({
+    render: h => h(App),
+  }).$mount('#app');
